913e2314fc1ae382a1401b5e480ce8d8d1e58de9
Ernesto MR <ernestomrr2@gmail.com>
2019-05-26 17:12:36 -0400

    Update

---
+++ index.js
@@ -0,0 +1,211 @@
+var Crawler = require("crawler");
+const htmlToText = require('html-to-text');
+var parse = require('url-parse')
+var waterfall = require('async/waterfall');
+var map = require('async/map');
+var sites = require("./sites");
+
+
+class ArticlesClass {
+    constructor(json) {
+        this.json = json;
+        this.articles = [];
+    }
+
+    // Getter
+    get data() {
+        return this.data();
+    }
+
+    // Método
+    webScrapingFirstLvl(cb) {
+        const _this = this;
+        const c = new Crawler({
+            maxConnections: 10,
+            // This will be called for each crawled page
+            callback: (error, res, done) => {
+                if (error) {
+                    cb(error, null);
+                } else {
+                    var $ = res.$;
+
+                    const articles = [];
+                    if (res.options.article.root) {
+                        let rootSelector = res.options.article.root.number ?
+                            $(`${res.options.article.root.base}`).eq(res.options.article.root.number) :
+                            $(`${res.options.article.root.base}`);
+
+
+                        rootSelector.each(function (i, elem) {
+                            let url = res.options.article.url.number ?
+                                $(this).find(`${res.options.article.url.base}`).eq(res.options.article.url.number).attr('href') :
+                                $(this).find(`${res.options.article.url.base}`).attr('href');
+
+                            let image = res.options.article.image.number ?
+                                $(this).find(`${res.options.article.image.base}`).eq(res.options.article.image.number).attr('src') :
+                                $(this).find(`${res.options.article.image.base}`).attr('src');
+                            if (!image) {
+                                image = res.options.article.image.number ?
+                                    $(this).find(`${res.options.article.image.base}`).eq(res.options.article.image.number).attr('data-src') :
+                                    $(this).find(`${res.options.article.image.base}`).attr('data-src');
+                            }
+
+
+
+                            let title = res.options.article.title.number ?
+                                $(this).find(`${res.options.article.title.base}`).eq(res.options.article.title.number).text() :
+                                $(this).find(`${res.options.article.title.base}`).text();
+                            title = htmlToText.fromString(title);
+
+                            let summary = res.options.article.summary.number ?
+                                $(this).find(`${res.options.article.summary.base}`).eq(res.options.article.summary.number).text() :
+                                $(this).find(`${res.options.article.summary.base}`).text();
+                            summary = htmlToText.fromString(summary);
+
+
+                            let aux = {
+                                title,
+                                url: parse(res.options.base && !url.includes("http") ? res.options.base + "" + url : url).href,
+                                image: parse(image && res.options.base && !image.includes("http") ? res.options.base + "" + image : image).href,
+                                summary,
+                            }
+                            if (aux.title && aux.url) {
+                                articles.push(aux);
+                            }
+                        });
+                    }
+                    _this.articles = articles;
+
+                    cb(null, true);
+                }
+                done();
+            }
+        });
+        c.queue({
+            uri: this.json.url,
+            article: this.json.article,
+            base: this.json.base || null,
+        })
+
+    }
+
+    webScrapingSecondLvl(url, selector, cb) {
+        const c = new Crawler({
+            maxConnections: 10,
+            callback: (error, res, done) => {
+                if (error) {
+                    cb(error, null)
+                } else {
+                    var $ = res.$;
+                    let html = res.options.number ?
+                        $(`${res.options.base}`).eq(res.options.number).html() :
+                        $(`${res.options.base}`).html();
+                    let text = res.options.number ?
+                        $(`${res.options.base}`).eq(res.options.number).text() :
+                        $(`${res.options.base}`).text();
+                    text = htmlToText.fromString(text);
+
+                    cb(null, { html, text });
+                }
+                done();
+            }
+        });
+        c.queue({
+            uri: url,
+            base: selector.base,
+            number: selector.number,
+        })
+    }
+
+    exec(cb) {
+        this.webScrapingFirstLvl((err, result) => {
+            if (err && result) {
+                cb(err, null);
+            } else {
+                map(this.articles, (article, cbm) => {
+                    this.webScrapingSecondLvl(encodeURI(article.url), this.json.article.body, (err, result) => {
+                        if (err) {
+                            cbm(err, null);
+                        } else {
+                            if (result.html) {
+                                article = Object.assign(article, result);
+                            }
+                            cbm(null, article);
+                        }
+                    });
+                }, (err, result) => {
+                    if (err) {
+                        cb(err, null);
+                    }
+                    else {
+                        const array = result.filter((item) => {
+                            return item.html;
+                        })
+                        cb(err, array);
+                    }
+
+                })
+            }
+        })
+    }
+
+}
+
+
+let ko = 0;
+
+map(sites, (site, cbm) => {
+    const articles = new ArticlesClass(site);
+    articles.exec((err, result) => {
+        if (err) {
+            ko++;
+            cbm(null, site);
+        } else {
+            site.data = result;
+            cbm(null, site);
+        }
+    });
+}, (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        result.map((source) => {
+            console.log("*******************************************");
+            console.log("*******************************************");
+
+            source.data.map((article) => {
+                console.log("-------- Title --------");
+                console.log(article.title);
+                console.log("-------- URL --------");
+                console.log(article.url);
+                console.log("-------- TEXT --------");
+                console.log(article.text);
+            })
+            console.log("*******************************************");
+            console.log("*******************************************");
+        })
+
+        console.log(result.length);
+    }
+});
+
+
+// var c = new Crawler({
+//     maxConnections : 10,
+//     // This will be called for each crawled page
+//     callback : function (error, res, done) {
+//         if(error){
+//             console.log(error);
+//         }else{
+//             var $ = res.$;
+//             // $ is Cheerio by default
+//             //a lean implementation of core jQuery designed specifically for the server
+//             console.log($("div.xsmall-12.small-12.medium-8.large-8.columns").eq(1).text());
+//         }
+//         done();
+//     }
+// });
+
+// // Queue just one URL, with default callback
+// c.queue('https://www.festo.com/group/es/cms/13591.htm');
+
---
+++ sites.js
@@ -0,0 +1,330 @@
+const sites = [
+    {
+        "url": "https://www.festo.com/group/es/cms/11757.htm",
+        "base": "https://www.festo.com/group/es/cms",
+        "article": {
+            "root": {
+                "base": "section.teaser",
+                "number": 0
+            },
+            "title": {
+                "base": "h2 a",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": "p",
+                "number": 0
+            },
+            "body": {
+                "base": "div.box",
+                "number": 2
+            },
+        }
+    },
+    {
+        "url": "http://blog.stratasys.com",
+        "article": {
+            "root": {
+                "base": "article",
+                "number": 0
+            },
+            "title": {
+                "base": ".entry-title.h3",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": ".entry-contentp",
+                "number": 0
+            },
+            "body": {
+                "base": ".entry-content",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.salesforce.com/company/news-press/press-releases",
+        "article": {
+            "root": {
+                "base": "div.cardComponent",
+                "number": 0
+            },
+            "title": {
+                "base": "span.header-text",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": ".entry-contentp",
+                "number": 0
+            },
+            "body": {
+                "base": ".bodyCopyComponent",
+                "number": 0
+            }
+        }
+    },
+    {
+        "url": "https://www.genewsroom.com",
+        "base": "https://www.genewsroom.com",
+        "article": {
+            "root": {
+                "base": ".geui-card-inner",
+                "number": 0
+            },
+            "title": {
+                "base": "p",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": "a",
+                "number": 0
+            },
+            "body": {
+                "base": "div.content",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.salesforce.com/blog",
+        "base": "https://www.salesforce.com",
+        "article": {
+            "root": {
+                "base": "article.article-listing",
+                "number": 0
+            },
+            "title": {
+                "base": "p",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": "p.article-listing_excerpt",
+                "number": 0
+            },
+            "body": {
+                "base": "div.padding-50-right-lg.line-height-17",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://blogs.microsoft.com/",
+        "base": "https://www.salesforce.com",
+        "article": {
+            "root": {
+                "base": "section article",
+                "number": 0
+            },
+            "title": {
+                "base": "h3",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": null,
+                "number": 0
+            },
+            "body": {
+                "base": ".entry-content",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.bosch-presse.de/pressportal/de/en/news",
+        "article": {
+            "root": {
+                "base": "div.theme-wrapper",
+                "number": 0
+            },
+            "title": {
+                "base": "h3.theme-title",
+                "number": 0
+            },
+            "url": {
+                "base": "a.theme-container",
+                "number": 0
+            },
+            "image": {
+                "base": "img.theme-image",
+                "number": 0
+            },
+            "summary": {
+                "base": null,
+                "number": 0
+            },
+            "body": {
+                "base": "article.article-content",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.festo.com/net/en_corp/SupportPortal/press.aspx?tab=10&s=t",
+        "base": "https://www.festo.com",
+        "article": {
+            "root": {
+                "base": "table.ResultTable tbody tr",
+                "number": 0
+            },
+            "title": {
+                "base": "span.headline",
+                "number": 0
+            },
+            "url": {
+                "base": "a.link",
+                "number": 0
+            },
+            "image": {
+                "base": "img.itemImage",
+                "number": 0
+            },
+            "summary": {
+                "base": "div.expander",
+                "number": 0
+            },
+            "body": {
+                "base": "#ctl00_a_cphMain_a_divDescription",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://news.microsoft.com/",
+        "article": {
+            "root": {
+                "base": "div.theme-dark",
+                "number": 0
+            },
+            "title": {
+                "base": "h2",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": null,
+                "number": 0
+            },
+            "body": {
+                "base": "section",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.blog.kuka.com/?lang=en",
+        "article": {
+            "root": {
+                "base": ".grid-sidebar .box",
+                "number": 0
+            },
+            "title": {
+                "base": "h3 a",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": 'p',
+                "number": 0
+            },
+            "body": {
+                "base": ".post_content",
+                "number": 0
+            },
+        }
+    },
+    {
+        "url": "https://www.medtronic.com/us-en/about/news.html",
+        "base": "https://www.medtronic.com",
+        "article": {
+            "root": {
+                "base": ".textclearfloat",
+                "number": 0
+            },
+            "title": {
+                "base": "h3",
+                "number": 0
+            },
+            "url": {
+                "base": "a",
+                "number": 0
+            },
+            "image": {
+                "base": "img",
+                "number": 0
+            },
+            "summary": {
+                "base": 'p',
+                "number": 0
+            },
+            "body": {
+                "base": "section",
+                "number": 0
+            },
+        }
+    },
+]
+
+module.exports = sites;
+
